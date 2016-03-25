42d500fff8cc16d876909c2a6575e9bfc41b9caa
Jackson Cafazzo <doctorjuno@gmail.com>
2016-03-24 17:47:53 -0700

    add initial files with a skeleton styles.scss

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>HOWDYHOWDYHO</h1>
+    <hr>
+
+  </div>
+  `
+})
+
+export class AppComponent {
+  
+}
